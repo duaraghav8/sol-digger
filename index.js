d22870ea5edbdc9214122c13db867ab1ea222410
duaraghav8@gmail <duaraghav8@gmail.com>
2016-08-08 09:56:30 +0530

    Initial commit - module seems to work hmff

---
+++ index.js
@@ -0,0 +1,46 @@
+/**
+ * @fileoverview Function to recursively explore the given directory to discover all .sol files
+ * @author Raghav Dua <duaraghav8@gmail.com>
+ */
+
+'use strict';
+
+var path = require ('path'),
+	fs = require ('fs');
+
+function traverse (currentDir, allFiles, ignore) {
+
+	var currentDirItems;
+	var SOL_EXT = '.sol';
+
+	if (!path.isAbsolute (currentDir)) {
+		currentDir = path.join (process.cwd (), currentDir);
+	}
+
+	currentDirItems = fs.readdirSync (currentDir);
+
+	currentDirItems.forEach (function (item) {
+		var absoluteItemPath = path.join (currentDir, item);
+
+		if (ignore && ignore.indexOf (item) > -1) {
+			return;
+		}
+
+		if (fs.lstatSync (absoluteItemPath).isDirectory ()) {
+
+			traverse (absoluteItemPath, allFiles, ignore);
+
+		} else if (path.extname (absoluteItemPath) === SOL_EXT) {
+
+			allFiles.push (absoluteItemPath);
+
+		}
+	});
+}
+
+module.exports = function dig (dir, ignore) {
+	var allFiles = [];
+
+	traverse (dir, allFiles, ignore);
+	return allFiles;
+};
